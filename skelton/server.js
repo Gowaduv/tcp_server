2b9845694824eee3a66db428051fd36c50a60448
KMSkelton <kmskelton@outlook.com>
2016-04-20 21:49:41 -0700

    add output to browser

--- skelton/server.js
+++ skelton/server.js
@@ -4,11 +4,11 @@ const fs = require('fs');
 const server = module.exports = net.createServer((socket) => {
   socket.on('data', (chunk) => {
     fs.writeFile(__dirname + '/response/' + Date.now() + '.txt', chunk, () => {
+      socket.write('FIN.');
       socket.end();
-      console.log('Deltron is FIN.');
     });
   });
 });
 server.listen(3030, () => {
-  console.log('Deltron listening on 3030. \n');
+  console.log('listening on 3030. \n');
 });
